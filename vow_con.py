e08b2608541c41d16ca3cfcf0327cda1335ef2ff
dhiva1201 <50898581+dhiva1201@users.noreply.github.com>
2019-05-23 07:10:56 +0530

    Create vow_con.py

---
+++ vow_con.py
@@ -0,0 +1,8 @@
+c=input()
+if(c>='a'and c<='z'or c>='A' and c<='Z'):
+    if(c=='a'or c=='e'or c=='i'or c=='o'or c=='u'or c=='A'or c=='E'or c=='I'or c=='O'or c=='U'):
+        print("Vowel")
+    else:
+        print("Consonant")
+else:
+    print("invalid")
