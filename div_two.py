182e5b937fac8991e990858fe3cf3e51ddeffa64
dhiva1201 <50898581+dhiva1201@users.noreply.github.com>
2019-05-23 15:38:10 +0530

    Update div_two.py

--- div_two.py
+++ div_two.py
@@ -1,7 +1,10 @@
 N=int(input())
-while((N%2)==0):
-    N=N/2
-    print(int(N))
-
+if((N%2)==0):
+    while((N%2)==0):
+        N=N/2
+        print(int(N))
+else:
+    print(N)
     
-#given a no. N,print the number obtained by dividing it by 2 recursively till it is not divisible
+
+#given a no. N,print the number obtained by dividing it by 2 recursively till it is not divisibl
