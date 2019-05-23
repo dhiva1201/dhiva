c3914685485269f21c3793cdae051e30fca46ddd
dhiva1201 <50898581+dhiva1201@users.noreply.github.com>
2019-05-23 15:06:09 +0530

    Update vow_con.py

--- vow_con.py
+++ vow_con.py
@@ -1,6 +1,7 @@
 c=input()
-if(c>='a'and c<='z'or c>='A' and c<='Z'):
-    if(c=='a'or c=='e'or c=='i'or c=='o'or c=='u'or c=='A'or c=='E'or c=='I'or c=='O'or c=='U'):
+if(c.isalpha()):
+    c=c.lower()
+    if(c=='a'or c=='e'or c=='i'or c=='o'or c=='u'):
         print("Vowel")
     else:
         print("Consonant")
