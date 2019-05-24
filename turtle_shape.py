4934a6659d2af5440797a35bbb1c133a21a2b3ef
dhiva1201 <50898581+dhiva1201@users.noreply.github.com>
2019-05-24 15:29:27 +0530

    Create turtle_shape.py

---
+++ turtle_shape.py
@@ -0,0 +1,17 @@
+import turtle               # allows us to use the turtles library
+wn = turtle.Screen()        # creates a graphics window
+wn.setup(540,508)           # set window dimension
+
+alex = turtle.Turtle()      # create a turtle named alex
+alex.shape("turtle")        # alex looks like a turtle
+alex.color('blue')           # alex has a color
+
+alex.circle(50)              # draws a circle of radius 50
+alex.forward(100)            # alex moves 50 positions backward
+alex.backward(200)             # alex moves 50 positions forward
+alex.left(90)               # alex turns 60 degrees right
+alex.forward(100) 
+alex.right(90)
+alex.forward(200) 
+alex.right(90)                # alex turns 60 degrees left
+alex.forward(100)
