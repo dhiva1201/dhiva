c6fec48a1a079a88bd275930bcb86ef6db40eb5a
dhiva1201 <50898581+dhiva1201@users.noreply.github.com>
2019-05-31 22:11:58 +0530

    Update print_same_letters_of_string_in_order.py

--- print_same_letters_of_string_in_order.py
+++ print_same_letters_of_string_in_order.py
@@ -1,12 +1,12 @@
 n=input()
 n=int(n)#4
 l=[]
-for i in range(0,n):  #dhivakar dhivagar emaya durga
+for i in range(0,n):  
     n1=input()
     l.append(n1)
 Z=[]
-for i in zip(*l): #i=('d', 'd', 'e', 'd')
-    if i.count(i[0])==len(i): #3==4
+for i in zip(*l):
+    if i.count(i[0])==len(i): 
         Z.append(i[0])
     else:
         break
